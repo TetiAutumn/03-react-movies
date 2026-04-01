8f7705607296032dffe5f29524ede08c20aa1f24
Tetiana <@email.com>
2026-04-01 22:12:32 +0200

    firstpars

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,46 +1,72 @@
 
-import { useEffect } from "react";
+
 import toast, { Toaster } from "react-hot-toast";
 import SearchBar from "../SearchBar/SearchBar";
+import { MovieGrid } from "../MovieGrid/MovieGrid";
 import "./App.module.css"
 import { movieService } from "../../services/movieService";
-
+import { useState, useEffect, use } from "react";
+import type { Movie } from "../../types/movie";
+import { Loader } from "../Loader/Loader";
 
 export default function App() {
 
-  useEffect(() => {
-    // movieService();
-  }, []);
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
 
   const handleSubmitSearch = async (event: React.SubmitEvent<HTMLFormElement>) => {
     event.preventDefault();
+
     const form = event.currentTarget;
 
     const formData = new FormData(form);
 
-    const query = formData.get('query') as string;
+    try {
+      setIsLoading(true);
+      const query = formData.get('query') as string;
+      if (query === '') {
+        toast('Please enter your search query');
+        return;
+      }
+      const data = await movieService(query);
 
-    if (query === '') {
-      toast('Please enter your search query');
-      return;
+      if (data?.total_results === 0) {
+        toast('No movies found for your request.');
+        setMovies([]);
+        return;
+      }
+
+      if (data) {
+        setMovies(data.results);
+      }
+    } finally {
+      setIsLoading(false);
     }
-
-    const movies = await movieService(query);
-
-    if (movies?.total_results === 0) {
-      toast('No movies found for your request.');
-
-    }
-    console.log(movies);
-
 
 
     form.reset();
   }
+
+  useEffect(() => {
+    console.log('movies', movies);
+    console.log('isLocading', isLoading);
+
+  }, [movies, isLoading]);
+
   return (
     <>
       <SearchBar onSubmit={handleSubmitSearch} />
       <Toaster />
+      {isLoading && <Loader />}
+      {!isLoading && movies.length > 0 && (
+        <MovieGrid
+          movies={movies}
+          onSelect={(movie) => console.log("Selected:", movie)}
+        />)
+      }
+
     </>
   );
+
 }
